312c05350ccd507f1de06add6de5d885763cdbd0
Justin <moojustin9@gmail.com>
2024-11-27 16:31:45 -0600

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -55,6 +55,40 @@ class Employee:
 
   #Function to update employee contact details 
   def update_employee_contact(cls, employee_id)
+    employee = cls.employee_finder(employee_id)
+    if employee:
+      print(f" Updating contact details for {employee.employee_name}: ")
+      new_phone = input(f"Enter new phone number(Current : {employee.phone_number}): ")
+      new_email = input(f"Enter new email (Current : employee.email}): " )
+      employee.phone_number = new_phone
+      employee.email = new_email
+      print(f"Contact details for {employee.employee_name} updated successfully!")
+    else:
+      print("Employee not found.")
+
+
+  #Locates an employee by using their ID number, this will help with updating details type shit
+  def employee_finder(cls, employee_id):
+    for employee in cls.employee_list:
+      if employee.employee_id == employee_id:
+        return employee
+      return None
+
+
+  #View attendance of employees funcion
+  def attendance_viewer(cls, employee_id)
+    employee = cls.employee_finder(employee_id)
+    if employee:
+      print(f"Attendance records for {employee.employee_name} : ")
+      if employee.attendance_r:
+        for idx, record in enumerate(employee.attendance_r, 1):
+          check_in, check_out = record
+          print(f"day {idx} Checked in at {check_in} | Checked out at {check_out if check_out else 'Not checked out yet'}")
+      else:
+        print("No attendance records found.")
+    else:
+      print("Employee not found.")
+          
   
   def __init__(self, employee_id, employee_name, employee_position, employee_username, employee_password, status = "Active"): 
     self.employee_id = employee_id
@@ -65,6 +99,7 @@ class Employee:
     self.status = status
     self.phonenumber = phone_number
     self.email = email
+    self.attendance_r = []
     
     Employee.employee_list.append(self)
 
