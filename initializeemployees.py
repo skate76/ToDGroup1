39ee797c8a9cfe49d2859320f2990f8cbc5b923f
Justin <moojustin9@gmail.com>
2024-11-27 14:05:35 -0600

    initializeemployees.py

---
+++ initializeemployees.py
@@ -0,0 +1,9 @@
+from employee import Employee
+def employee_initialization():
+  #Add the roles you want I guess, these are just space holders type shit
+  Employee(1, "Justin Moo", "Manager", "employee1", "password1")
+  Employee(2, "Katelyn Tait", "Cashier", "employee2", "password2")
+  Employee(3, "Nasya Burrell", "Cashier", "employee3", "password3")
+  Employee(4, "Tishawn Whyte", "Cashier", "employee4", "password4")
+  Employee(5, "Johnathon Bennet", "Cashier", "employee5", "password5")
+  Employee(6, "Dominic Adams", "Cashier", "employee6", "password6")
